6a4faaa2cf706b55b9b9a7ac6aa411cf74ba5df5
fykabutch <fuckabuka@mail.ru>
2024-03-02 11:10:18 +0500

    Пункт №3. Настройка файла .gitignore

---
+++ TRPO-Lab3.ClassLibrary/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace TRPO_Lab3.ClassLibrary
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ TRPO-Lab3.TestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TRPO_Lab3.TestProject
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
